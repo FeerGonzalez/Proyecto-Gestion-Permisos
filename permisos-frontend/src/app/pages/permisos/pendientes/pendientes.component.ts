34daa5b3d3c5c5e3ac386dd37bc3ae7faa0c538d
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2026-01-31 19:45:29 -0300

    Create pendientes
    Se creo el .html .css y .ts de pendientes.component

---
+++ permisos-frontend/src/app/pages/permisos/pendientes/pendientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pendientes',
+  imports: [],
+  templateUrl: './pendientes.component.html',
+  styleUrl: './pendientes.component.css'
+})
+export class PendientesComponent {
+
+}
