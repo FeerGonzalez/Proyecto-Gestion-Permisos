c192f9d5c90936789143f6d9720085f9dbfcbd97
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2026-02-02 12:44:31 -0300

    Update api.php
    Se elimino la ruta de user destroy

--- permisos-backend/routes/api.php
+++ permisos-backend/routes/api.php
@@ -41,7 +41,6 @@ Route::middleware(['auth:sanctum', 'role:rrhh'])->prefix('usuarios')->group(func
     Route::post('/', [UserController::class, 'store']);
     Route::get('/{user}', [UserController::class, 'show']);
     Route::put('/{user}', [UserController::class, 'update']);
-    Route::delete('/{user}', [UserController::class, 'destroy']);
     Route::patch('/{user}/desactivar', [UserController::class, 'desactivar']);
     Route::patch('/{user}/activar', [UserController::class, 'activar']);
 });
