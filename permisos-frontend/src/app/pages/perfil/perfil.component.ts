e0123a149e6a28a626b46671e0154dd985bda643
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2026-02-02 21:40:59 -0300

    Update perfil.component
    Se realizo la vista para el perfil.component incluyendo sus tres partes .html .css y .ts

--- permisos-frontend/src/app/pages/perfil/perfil.component.ts
+++ permisos-frontend/src/app/pages/perfil/perfil.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    FormsModule
+  ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+
+  email = '';
+  currentPassword = '';
+  password = '';
+  passwordConfirm = '';
+
+  loading = false;
+  message = '';
+  error = '';
+
+  private apiUrl = 'http://localhost:8000/api/me';
+
+  constructor(
+    private auth: AuthService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    const user = this.auth.getUser();
+    this.email = user.email;
+  }
+
+  actualizarEmail() {
+    this.loading = true;
+    this.error = '';
+
+    this.http.put(this.apiUrl, { email: this.email }).subscribe({
+      next: () => {
+        this.message = 'Email actualizado';
+        this.loading = false;
+      },
+      error: err => {
+        this.error = err.error?.error || 'Error al actualizar email';
+        this.loading = false;
+      }
+    });
+  }
+
+  cambiarPassword() {
+    this.loading = true;
+    this.error = '';
+
+    this.http.put(`${this.apiUrl}/password`, {
+      current_password: this.currentPassword,
+      password: this.password,
+      password_confirmation: this.passwordConfirm
+    }).subscribe({
+      next: () => {
+        this.message = 'Contraseña actualizada';
+        this.loading = false;
+      },
+      error: err => {
+        this.error = err.error?.error || 'Error al cambiar contraseña';
+        this.loading = false;
+      }
+    });
+  }
 
 }
