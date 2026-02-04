96cef036d5eaaac037fa727a27cd9af58fd68fc8
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2026-02-04 09:29:55 -0300

    Update AuthController.php
    Se elimino una funcion que no se usaba

--- permisos-backend/app/Http/Controllers/AuthController.php
+++ permisos-backend/app/Http/Controllers/AuthController.php
@@ -6,33 +6,13 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 use App\Http\Resources\Dto\UserResource;
-use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\UpdateProfileRequest;
 use App\Http\Requests\Auth\ChangePasswordRequest;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest  $request)
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'role' => User::ROLE_EMPLEADO,
-        ]);
-
-        $token = $user->createToken('angular-token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Usuario registrado correctamente',
-            'token' => $token,
-            'user' => new UserResource($user),
-        ], 201);
-    }
-
     public function login(LoginRequest  $request)
     {
         $user = User::where('email', $request->email)->first();
